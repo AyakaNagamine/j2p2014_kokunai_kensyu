301af243f3d88d25ea7a71e9a876d89a93f3bac3
AyakaNagamine <riajuutahine@gmail.com>
2014-08-07 11:05:00 +0900

    Add some comments.

--- java_test/src/java_test/HelloWorld.java
+++ java_test/src/java_test/HelloWorld.java
@@ -4,8 +4,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I'm hungry!");
-		System.out.println("Hello world!");
+		System.out.println("I'm hungry!");//引数String型
+		System.out.println(10);//引数int型、多重定義
+		/*
+		 * System.out.printInt(10);
+			などのように設定すると引数に合わせて関数名がかわって便利が悪い。
+		 */
 	}
 
 }
