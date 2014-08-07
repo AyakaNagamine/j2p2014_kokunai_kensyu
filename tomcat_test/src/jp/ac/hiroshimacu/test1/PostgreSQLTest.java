21d6819d8e0a3acfc97f9d31b6d04d2163a25f7c
AyakaNagamine <riajuutahine@gmail.com>
2014-08-07 15:29:59 +0900

    import文追加

--- tomcat_test/src/jp/ac/hiroshimacu/test1/PostgreSQLTest.java
+++ tomcat_test/src/jp/ac/hiroshimacu/test1/PostgreSQLTest.java
@@ -7,6 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 /**
  * Servlet implementation class PostgreSQLTest
