c4b00097492340e6ab376956d890184a705af74d
AyakaNagamine <riajuutahine@gmail.com>
2014-08-08 11:17:26 +0900

    first / drawing lines black and red.

---
+++ Applet_Test2/src/Test10.java
@@ -0,0 +1,26 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+public class Test10 extends Applet implements MouseMotionListener {
+	int gx = 0;
+	int gy = 0;
+	Graphics Grp;
+	Button btnRed;
+	public void init(){
+		Grp = getGraphics();
+		addMouseMotionListener(this);
+		add(btnRed = new Button("ê‘§"));
+	}
+	public void mouseDragged(MouseEvent e) {
+		Grp.drawLine(gx,gy,e.getX(),e.getY());gx = e.getX();gy = e.getY();
+	}
+	public void mouseMoved(MouseEvent e) {
+		gx = e.getX();	gy = e.getY();
+	}
+	public boolean action(Event e,Object o){
+		if(o.equals("ê‘§")) {
+			Grp.setColor(new Color(255,0,0));
+		}
+		return true;
+	}
+}
